c71beebea043514d3e9d4ddc94ab6097f7eb84da
Chris <chris@breadchris.com>
2026-03-04 22:08:41 -0500

    Fix Vercel config: use native Next.js build, ignore assets

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  trailingSlash: true
+  }
 }
 
 module.exports = nextConfig
